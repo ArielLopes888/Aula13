5acc26f5cc90eeaa180cf09e155bc5c73c05ac73
Ariel Lopes <ariellopes888@gmail.com>
2022-06-21 16:28:15 -0300

    Conclusão do Enumerador

--- Aula13/Enumerador.cs
+++ Aula13/Enumerador.cs
@@ -8,6 +8,22 @@ namespace Aula13
 {
     internal class Enumerador
     {
-
+        
+        enum Status
+        {
+            Ok = 200,
+            Redirecionamento_Permanente = 301,
+            Nao_Encontrado = 404,
+            Nao_Autorizado = 405,
+            Servidor_Indisponivel = 503,
+            Tempo_Esgotado = 504
+        }
     }
 }
+
+//Código 200, Descrição: Ok.
+//Código 301, Descrição: Redirecionamento permanente.
+//Código 404, Descrição: Não encontrado.
+//Código 405, Descrição: Não autorizado.
+//Código 503, Descrição: Servidor indisponível.
+//Código 504, Descrição: Tempo esgotado. 

8a4da3355887e33d10c4a16144f4ed71d10fc8f2
Caroliny S Q G Cagnoni <cgcagnoni@gmail.com>
2022-06-21 16:50:31 -0300

    exercicio 2.2 - inicializacao

--- Aula13/Escola.cs
+++ Aula13/Escola.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace Aula13
 {
-    internal class Escola
+    public class Escola
     {
+        Dictionary<int, Aluno> Matriculados = new Dictionary<int, Aluno>();
+
+        public void CadastrarAluno()
+        {
+            Matriculados.Add(1, new Aluno()
+            {
+                Matricula = 1234,
+                Nome = "José",
+                Sobrenome = "Silva",
+                Telefone = 123456789,
+            });
+        }
+
+        public void RemoverAluno()
+        {
+            Matriculados.Remove(123, "José");
+        }
     }
 }
+
+//Cadastrar Aluno.
+//Remover Aluno.
+//Listar todas as Matrículas com Nomes de Alunos.
